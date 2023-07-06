3f63f50dc087b0eebbd1dcdda471cf02d0260918
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-07-06 15:55:46 +0700

    Poll

--- app/Http/Livewire/CreatePoll.php
+++ app/Http/Livewire/CreatePoll.php
@@ -2,13 +2,53 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [''];
+
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:1|max:10',
+        'options.*' => 'required|min:1|max:255'
+    ];
+
+    protected $messages = [
+        'options.*' => 'This option can\'t be emtpy'
+    ];
+
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+
+    public function updated($propertyName)
+    {
+        return $this->validateOnly($propertyName);
+    }
+
+    public function createPoll()
+    {
+        $this->validate();
+
+        Poll::create([
+            'title' => $this->title,
+        ])->options()->createMany(collect($this->options)->map(fn ($option) => ['name' => $option])->all());
+
+        $this->reset('title', 'options');
     }
 }
